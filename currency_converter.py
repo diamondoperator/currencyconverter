313fa75997472382ca39c6157c2d4af3a91aca23
hackermann20xx <hackerman20xx@gmail.com>
2021-07-29 21:46:55 -0400

    add input function

--- currency_converter.py
+++ currency_converter.py
@@ -4,9 +4,12 @@ let Euro = Usd * 0.85
 let Yen = Usd * 110.08
 let Pounds = Usd * 0.72
 let Australian Dollar = Usd * 1.36
-let Usd = 0
+
 
 
 print("Welcome to the currency converter")
 Print("This console program will convert USD to Euros, Yen, British Pounds and Australian Dollars"
 )
+print("------------------------------------------------------------------------------------------")
+print("Please enter the amount of United States Dollars you would like to convert: ")
+let Usd = input()

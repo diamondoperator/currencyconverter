89b7b7330b4606d34497f7f611dc5c18b7863a59
hackermann20xx <hackerman20xx@gmail.com>
2021-07-31 23:56:29 -0400

    test working

--- currency_converter.py
+++ currency_converter.py
@@ -1,30 +1,35 @@
 #console program that converts USD to other top currencies
 
-let Euro = Usd * 0.85
-let Yen = Usd * 110.08
-let Pounds = Usd * 0.72
-let Australian Dollar = Usd * 1.36
+start_over = 1
+while start_over < 2:
+  print("Welcome to the currency converter")
+  print("This console program will convert USD to Euros" +","+ "Yen"+","+ "British Pounds and Australian Dollars")
+  print("------------------------------------------------------------------------------------------")
+  print("Please enter the amount of United States Dollars you would like to convert: ")
+  Usd = input()
+  Euro  = round(float(Usd) * 0.85,2)
+  Yen = round(float(Usd) * 110.08,2)
+  Pounds = round(float(Usd) * 0.72,2)
+  Australian_Dollar = round(float(Usd) * 1.36,2)
+  print("\n")
+  print("$"+ str(Usd) + " is equal to: ")
+  print(str(Euro) + " Euros")
+  print(str(Yen) + " Yen")
+  print(str(Pounds) + " British Pounds")
+  print(str(Australian_Dollar) + " Australian Dollars")
+  print("------------------------------------------------------------------------------------------")
 
-let response = y
-while response = y:
-print("Welcome to the currency converter")
-Print("This console program will convert USD to Euros, Yen, British Pounds and Australian Dollars"
-)
-print("------------------------------------------------------------------------------------------")
-print("Please enter the amount of United States Dollars you would like to convert: ")
-let Usd = input()
-print("$"+ Usd + " is equal to: ")
-print(Euro + "Euros")
-print(Yen + "Yen")
-print(Pounds + "British Pounds")
-print(Australian Dollar + "Australian Dollars")
-print("------------------------------------------------------------------------------------------")
 
-print("/n")
-print("Do you want to make another conversion? y/n ")
-let response = input()
+  question = input("Do you want to make another conversion? y/n: ")
+  print("\n")
 
-    if(response = "n")
-    {
-        print("Thanks for using this converter.")
-    }
+  if(question == "y"):
+    start_over = 1
+
+  else:
+    start_over = 2
+    print("Thanks for using this converter.")
+
+
+
+
